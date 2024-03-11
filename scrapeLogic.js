b2d9a42b194356a5619bc587c3ed68578e2234c4
Ishan <Braviaspan2002@gmail.com>
2024-03-11 14:51:37 +0530

    Updated logic

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -7,6 +7,7 @@ const scrapeLogic = async (req,res) => {
   var url = `https://news.google.com/search?q=%22${searchTerm}%22%20when%3A1d&hl=en-IN&gl=IN&ceid=IN%3Aen`
   const browser = await puppeteer.launch({
     headless:true,
+    defaultViewport: false,
     args: [
       "--disable-setuid-sandbox",
       "--no-sandbox",
@@ -18,41 +19,6 @@ const scrapeLogic = async (req,res) => {
         ? process.env.PUPPETEER_EXECUTABLE_PATH
         : puppeteer.executablePath(),
   });
-  // try {
-  //   const page = await browser.newPage();
-
-  //   await page.goto("https://news.google.com/search?q=meta%20when%3A1d&hl=en-IN&gl=IN&ceid=IN%3Aen");
-
-  //   const url = page.url()
-
-  //   // Set screen size
-  //   await page.setViewport({ width: 1080, height: 1024 });
-
-  //   // Type into search box
-  //   // await page.type(".search-box__input", "automate beyond recorder");
-
-  //   // Wait and click on first result
-  //   // const searchResultSelector = ".search-box__link";
-  //   // await page.waitForSelector(searchResultSelector);
-  //   // await page.click(searchResultSelector);
-
-  //   // Locate the full title with a unique string
-  //   // const textSelector = await page.waitForSelector(
-  //   //   "text/Customize and automate"
-  //   // );
-  //   // const fullTitle = await textSelector.evaluate((el) => el.textContent);
-
-  //   // // Print the full title
-  //   // const logStatement = `The title of this blog post is ${fullTitle}`;
-  //   // console.log(logStatement);
-  //   res.send(url);
-  // } catch (e) {
-  //   console.error(e);
-  //   res.send(`Something went wrong while running Puppeteer: ${e}`);
-  // } finally {
-  //   await browser.close();
-  // }
-
 
   try {
     const page = await browser.newPage();
@@ -65,9 +31,6 @@ const scrapeLogic = async (req,res) => {
       console.log("Continuing without full load")
     }
     
-    await page.setViewport({ width: 1080, height: 1024 });
-
-
     newsList = await page.$$eval('.JtKRv', elements => {
       return elements.map(element => {
         return {
